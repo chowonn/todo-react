4d0c23753e4218f4e83dad3a53a99ae1df0c4abb
chowonn <hcw12321@gmail.com>
2023-10-10 14:45:17 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+function App() {
+  return (
+    <>
+      <div className=" text-3xl font-bold text-gray-600">dsdsdsds</div>
+    </>
+  );
+}
+
+export default App;
