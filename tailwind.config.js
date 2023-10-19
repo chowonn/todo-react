c0489c9cf0e9f89c951cc700ead62974a0ed991f
chowonn <hcw12321@gmail.com>
2023-10-19 17:34:53 +0900

    [Add] background 사용자 정의 컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.js', './src/**/*.jsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gradient-from': '#7F7FD5',
+        'gradient-via': '#86A8E7',
+        'gradient-to': '#91EAE4',
+      },
+    },
   },
   plugins: [],
 };
