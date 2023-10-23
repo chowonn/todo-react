36997a89eb2d909f7e3ab843e0b83c8206b21818
chowonn <hcw12321@gmail.com>
2023-10-23 17:53:22 +0900

    [Add] TodoHead 마크업 및 스타일링 완료

---
+++ src/components/TodoHead.jsx
@@ -0,0 +1,11 @@
+function TodoHead() {
+  return (
+    <div>
+      <h1 className="mt-[100px] text-6xl font-bold text-white">
+        췅님 &apos;s Todo List
+      </h1>
+    </div>
+  );
+}
+
+export default TodoHead;
