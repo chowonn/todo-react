c1ecf71e745bc15befe8017317098f5c982a3de8
chowonn <hcw12321@gmail.com>
2023-10-25 18:47:34 +0900

    [Add] item 항목 보이도록 수정

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,14 +2,24 @@ import { MdEdit } from 'react-icons/md';
 import { IoClose } from 'react-icons/io5';
 import { GoCheckCircle } from 'react-icons/go';
 import { GoCheckCircleFill } from 'react-icons/go';
+import PropTypes from 'prop-types';
 
-function TodoItem() {
+function TodoItem({ todoItem, todoList, setTodoList }) {
+  const { checked } = todoList;
   return (
     <>
       <li className=" flex items-center justify-between ">
         <div className="flex gap-[10px]">
-          <GoCheckCircle size="28" className="mr-[10px] inline-block" />
-          <p>코딩테스트 2문제 풀기</p>
+          {checked ? (
+            <GoCheckCircle
+              size="28"
+              className="mr-[10px] inline-block"
+              color="white"
+            />
+          ) : (
+            <GoCheckCircleFill size="28" className="mr-[10px] inline-block" />
+          )}
+          <p>{todoItem.text}</p>
         </div>
         <div className="flex gap-[10px]">
           <button className="">
@@ -23,5 +33,19 @@ function TodoItem() {
     </>
   );
 }
+
+TodoItem.propTypes = {
+  todoItem: PropTypes.shape({
+    id: PropTypes.number,
+    text: PropTypes.string.isRequired,
+  }),
+  todoList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ),
+  setTodoList: PropTypes.func.isRequired,
+};
 
 export default TodoItem;
