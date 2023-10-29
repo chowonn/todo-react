3703ed0533c7fbd599f86f2806a8cde50582a8b5
chowonn <hcw12321@gmail.com>
2023-10-29 20:36:00 +0900

    [Feat] 전체삭제 기능 추가

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,20 @@
+import { useCallback } from 'react';
 import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
+// import { useCallback } from 'react';
 
 function TodoList({ todoList, setTodoList }) {
+  const handleDeleteAll = useCallback(() => {
+    setTodoList([]);
+  });
   return (
     <>
       <h3 className="sr-only">Todo List</h3>
       <div className="relative ml-11 h-[600px] w-[500px] rounded-[28px] bg-white px-9 py-[27px] opacity-80">
         <button
           type="button"
-          className="text-red absolute right-[25px] top-[25px] text-sm"
+          className="absolute right-[25px] top-[25px] text-sm text-red"
+          onClick={handleDeleteAll}
         >
           전체삭제
         </button>
@@ -35,6 +41,7 @@ TodoList.propTypes = {
     }).isRequired
   ),
   setTodoList: PropTypes.func.isRequired,
+  handleDeleteAll: PropTypes.func,
 };
 
 export default TodoList;
