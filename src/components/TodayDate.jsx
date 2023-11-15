c6388c47ec02a85a4569594aa96f0cd12cd1697b
chowonn <hcw12321@gmail.com>
2023-11-15 20:34:20 +0900

    [Add] 반응형일 때 스타일 변경

--- src/components/TodayDate.jsx
+++ src/components/TodayDate.jsx
@@ -1,4 +1,9 @@
+import { useMediaQuery } from 'react-responsive';
+
 function TodayDate() {
+  const isSmallScreen = useMediaQuery({
+    query: '(max-width: 480px)',
+  });
   const today = new Date();
   const dateString = today.toLocaleDateString('ko-KR', {
     year: 'numeric',
@@ -6,11 +11,23 @@ function TodayDate() {
     day: 'numeric',
     weekday: 'long',
   });
+
   return (
     <>
-      <p className="text-left text-[28px] font-semibold text-white">
+      {!isSmallScreen ? (
+        <p className="text-left text-[28px] font-semibold text-white">
+          {dateString}
+        </p>
+      ) : (
+        <p className="ml-[25px] mt-[33px]  text-[18px] font-medium text-white ">
+          {dateString}
+        </p>
+      )}
+      {/* <p className="text-left text-[28px] font-semibold text-white">
         {dateString}
-      </p>
+      </p> */}
+
+      {/* <p className="ml-[25px] mt-[33px]">{dateString}</p> */}
     </>
   );
 }
