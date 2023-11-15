c8659dd858c772c29d65d0b6d3682b0013cad25e
chowonn <hcw12321@gmail.com>
2023-11-15 21:14:03 +0900

    [Modify] 가운데로 정렬

--- src/components/TodoHead.jsx
+++ src/components/TodoHead.jsx
@@ -8,7 +8,7 @@ function TodoHead() {
   return (
     !isSmallScreen && (
       <div>
-        <h1 className="mt-[100px] text-6xl font-bold text-white">
+        <h1 className="mt-[100px] text-center text-6xl font-bold text-white">
           췅님 &apos;s Todo List
         </h1>
       </div>
