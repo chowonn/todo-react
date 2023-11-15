5777883d4c81f7879ba9e0f2199e943e1771b2fb
chowonn <hcw12321@gmail.com>
2023-11-15 19:50:23 +0900

    [Add] 뷰포트 480px 이하일 때 숨김 처리

--- src/components/TodoHead.jsx
+++ src/components/TodoHead.jsx
@@ -1,10 +1,18 @@
+import { useMediaQuery } from 'react-responsive';
+
 function TodoHead() {
+  const isSmallScreen = useMediaQuery({
+    query: '(max-width: 480px)',
+  });
+
   return (
-    <div>
-      <h1 className="mt-[100px] text-6xl font-bold text-white">
-        췅님 &apos;s Todo List
-      </h1>
-    </div>
+    !isSmallScreen && (
+      <div>
+        <h1 className="mt-[100px] text-6xl font-bold text-white">
+          췅님 &apos;s Todo List
+        </h1>
+      </div>
+    )
   );
 }
 
