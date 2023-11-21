ba0469b59bb5a3d508142a7060ab24fed0256917
chowonn <hcw12321@gmail.com>
2023-11-22 02:19:57 +0900

    [Add, Modify] 목업 데이터 추가 및 항목 추가 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,39 @@ import TodoHead from './components/TodoHead';
 import TodoInsert from './components/TodoInsert';
 import TodayDate from './components/TodayDate';
 import Clock from './components/Clock';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      text: '리액트 공부하기',
+      checked: false,
+    },
+    {
+      id: 2,
+      text: '두부 산책가기',
+      checked: false,
+    },
+    {
+      id: 3,
+      text: '코딩테스트 문제풀기',
+      checked: true,
+    },
+  ]);
+
+  const nextId = useRef(0);
+
+  const handleSubmit = (text) => {
+    const newTodo = todo.concat({
+      id: nextId.current,
+      text,
+      checked: false,
+    });
+    setTodo(newTodo);
+    nextId.current += 1;
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-gradient-from via-gradient-via to-gradient-to">
       <div className="m-auto flex w-[914px] flex-col justify-center gap-[72px]">
@@ -16,10 +45,10 @@ function App() {
             <TodayDate />
             <Clock />
           </div>
-          <TodoList todoList={todoList} setTodoList={setTodoList} />
+          <TodoList todo={todo} />
         </main>
 
-        <TodoInsert todoList={todoList} setTodoList={setTodoList} />
+        <TodoInsert todo={todo} onSubmit={handleSubmit} />
       </div>
     </div>
   );
